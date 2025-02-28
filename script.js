98e543e382c7237293c3cc10af5856ff7c5c1d72
Prashanth2025 <prashanthramesh.02@gmail.com>
2025-02-28 09:32:37 +0530

    Fixed resume download and styling

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Smooth Scroll Functionality
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({
         behavior: 'smooth'
     });
 }
 
-// Scroll Reveal Animation
+
 document.addEventListener('scroll', function () {
     const sections = document.querySelectorAll('section');
 
